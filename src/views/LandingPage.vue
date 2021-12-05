800d28c2535f540f8b91fbffd32f0e2551b6b865
a.m.jacome <a.m.jacomep@hotmail.com>
2021-12-04 23:53:38 -0600

    Dashboard y Laning page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,17 +1,23 @@
 <template>
 
   <!-- ENCABEZADO -->
-  <header>
-   
-  </header>
 
   <!-- NavBar -->
-  <nav class="navbar navbar-light mb-5">
+
+  <div class="custom-shape-divider-top-1638680168">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" class="shape-fill"></path>
+      </svg>
+    </div>
+
+  <nav class="navbar navbar-light mt-5 mb-5">
     <a class="navbar-brand" href="#">
       <img src="../assets/logoElements/Logo-Horizontal-Completo.png" width="240" height="58" class="d-inline-block align-cneter" alt="">
     </a>
   </nav>
-  
+
+
+
   <!-- Carusel -->
     <div id="carouselExampleSlidesOnly" class="carousel slide container" data-ride="carousel">
       <div class="carousel-inner">
@@ -27,14 +33,39 @@
       </div>
   </div>
 
-  
+  <div class="custom-shape-divider-bottom-1638680595">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" class="shape-fill"></path>
+    </svg>
+  </div>
 
-  <section class="w-50 mx-auto text-center pt-5" id="intro">
-    
-    <h1 class="p-3 fs-2 border-top border-3">Una agencia única para todas tus necesidades de <span class="text-primary">Marketing Digital</span></h1>
-    <p class="p-3 fs-4"><span class="text-primary">EVA.IO </span> es la agencia donde te ayudamos a establecer tu precencia online SEO paginas Web, tiendas virtuales redes sociales</p>
+  <div class="custom-shape-divider-top-1638683446">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+    </svg>
+  </div>
+
+ 
+  <section class="w-50 mx-auto text-center pt-1" id="intro">
+    <img class="mb-5" src="../assets/logoElements/iconEvaIO.png" alt="desarrollo">
+    <h1 class="p-3 fs-2 border-top border-3">Un espacio único para todas tus necesidades en cuanto al <span class="text-primary"> Reforzamiento de conocimeintos</span></h1>
+    <p class="p-3 fs-4"><span class="text-primary">Eva.io </span> es el portal donde puedes reforzar todo lo aprendido sobre tus clases de matemáticas </p>
   </section>
 
+  <div class="custom-shape-divider-bottom-1638683336">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+    </svg>
+  </div>
+
+  <div class="custom-shape-divider-top-1638683127">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+        <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+        <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+    </svg>
+  </div>
+  
   <!-- Servicios -->
   <section class="container-fluid">
     <div class="row w-74 mx-auto my-5 servicio-fila">
@@ -55,6 +86,8 @@
         </div>
       </div>
     </div>
+
+   
 
     <div class="row w-74 mx-auto my-5 servicio-fila">
       
@@ -128,6 +161,115 @@
 
 <style lang="scss" scoped>
 
+  /* 1 */  
+  .custom-shape-divider-top-1638680168 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: rotate(180deg);
+}
+
+.custom-shape-divider-top-1638680168 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 150px;
+}
+
+.custom-shape-divider-top-1638680168 .shape-fill {
+    fill: #039EB4;
+}
+
+/* 2 */  
+.custom-shape-divider-bottom-1638680595 {
+    position: inherit;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+}
+
+.custom-shape-divider-bottom-1638680595 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 150px;
+}
+
+.custom-shape-divider-bottom-1638680595 .shape-fill {
+    fill: #577E60;
+}
+
+/* 3 */  
+.custom-shape-divider-top-1638680907 {
+    position: inherit;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: rotate(180deg);
+}
+
+.custom-shape-divider-top-1638680907 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 140px;
+}
+
+.custom-shape-divider-top-1638680907 .shape-fill {
+    fill: #577E60;
+}
+
+/* 4 */  
+.custom-shape-divider-bottom-1638683336 {
+    position: inherit;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+}
+
+.custom-shape-divider-bottom-1638683336 svg {
+    position: relative;
+    display: block;
+    width: calc(228% + 1.3px);
+    height: 155px;
+    transform: rotateY(180deg);
+}
+
+.custom-shape-divider-bottom-1638683336 .shape-fill {
+    fill: #649C71;
+}
+
+/* 5 */  
+.custom-shape-divider-top-1638683127 {
+    position: inherit;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+}
+
+.custom-shape-divider-top-1638683127 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 100px;
+    transform: rotateY(180deg);
+}
+
+.custom-shape-divider-top-1638683127 .shape-fill {
+    fill: #649C71;
+}
+
   .carousel-inner
   {
     max-height: 39rem;
@@ -137,8 +279,6 @@
   {
     justify-content: center;
   }
-
-  
 
   @media (max-width:768px) {
     #intro, #equipo
